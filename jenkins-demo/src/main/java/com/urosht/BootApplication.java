fc1d78e544ede6e33d3e700daa58adf6d51d5818
Urosh Trifunovic <nediarovic@gmail.com>
2018-08-26 20:50:33 +0200

    Add controller

--- jenkins-demo/src/main/java/com/urosht/BootApplication.java
+++ jenkins-demo/src/main/java/com/urosht/BootApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RedisDemoApplication {
+public class BootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RedisDemoApplication.class, args);
+		SpringApplication.run(BootApplication.class, args);
 	}
 }
